acbd8f03383efab03a93d48a480e3a602b74b748
kirillYoYo <kirillmyasnikovmail@gmail.com>
2026-02-08 15:11:42 +0500

    add colors

--- src/game/scenes/GameScene.ts
+++ src/game/scenes/GameScene.ts
@@ -1,8 +1,10 @@
+import { Tile } from '@/game/entities/Tile'
+
 import { createInitialState } from '../state/createInitialState'
 import { handleTileClick } from '../systems/ClickSystem'
+import { TileView } from '../views/TileView'
 import { applyGravity } from '../systems/GravitySystem'
 import { refill } from '../systems/RefillSystem'
-import { TileView } from '../views/TileView'
 import { GameState } from '../state/state'
 
 export class GameScene extends Phaser.Scene {
@@ -11,25 +13,18 @@ export class GameScene extends Phaser.Scene {
 
     constructor() {
         super('GameScene')
+
+        this.tileClickHandler = this.tileClickHandler.bind(this)
     }
 
     create() {
         this.state = createInitialState(8, 8)
         this.render()
-
-        this.input.on('pointerdown', (p: Phaser.Input.Pointer) => {
-            const x = Math.floor(p.x / 64)
-            const y = Math.floor(p.y / 64)
-            console.log('px', p.x)
-            console.log('py', p.y)
-
-            this.state = handleTileClick(this.state, x, y)
-            this.state = applyGravity(this.state)
-            this.state = refill(this.state)
-
-            this.render()
-        })
     }
+
+    /**
+     * render
+     * */
 
     render() {
         for (const row of this.state.grid) {
@@ -38,12 +33,19 @@ export class GameScene extends Phaser.Scene {
 
                 let view = this.views.get(tile.id)
                 if (!view) {
-                    view = new TileView(this, tile)
+                    view = new TileView(this, tile, this.tileClickHandler)
                     this.views.set(tile.id, view)
                 }
                 view.update(tile)
             }
         }
+    }
+
+    tileClickHandler(tile: Tile) {
+        this.state = handleTileClick(this.state, tile.x, tile.y)
+        this.state = applyGravity(this.state)
+        this.state = refill(this.state)
+        this.render()
     }
 
     // update(time: number, delta: number) {
--- src/game/views/TileView.ts
+++ src/game/views/TileView.ts
@@ -1,11 +1,24 @@
 import { Tile } from '../entities/Tile'
 
+const COLORS = {
+    red: hexToTint('#c60f0f'),
+    green: hexToTint('#1a9c14'),
+    blue: hexToTint('#142b9c'),
+    yellow: hexToTint('#bac60fff'),
+} as const
+
+console.log("Phaser.Display.Color.HexStringToColor('#BAC60FFF')", COLORS)
+
 export class TileView {
     sprite: Phaser.GameObjects.Sprite
 
-    constructor(scene: Phaser.Scene, tile: Tile) {
-        this.sprite = scene.add.sprite(tile.x * 64, tile.y * 64, tile.color)
+    constructor(scene: Phaser.Scene, tile: Tile, clickClb: (tile: Tile) => void) {
+        this.sprite = scene.add.sprite(tile.x * 64, tile.y * 64, 'tile').setInteractive()
+        this.sprite.setTintFill(COLORS[tile.color])
         this.sprite.setOrigin(0)
+        this.sprite.on('pointerdown', function () {
+            clickClb(tile)
+        })
     }
 
     update(tile: Tile) {
@@ -15,4 +28,16 @@ export class TileView {
     destroy() {
         this.sprite.destroy()
     }
+}
+
+function hexToTint(hex: string) {
+    const rgb = hex.slice(0, 7)
+    return Phaser.Display.Color.HexStringToColor(rgb).color
+}
+
+function hexAlpha(hex: string) {
+    if (hex.length === 9) {
+        return parseInt(hex.slice(7, 9), 16) / 255
+    }
+    return 1
 }
